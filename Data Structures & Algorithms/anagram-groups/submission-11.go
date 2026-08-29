ad4be57c422daa49ba2f498719ebad8ca2f4fc4c
Rahul <42892759+heyrmi@users.noreply.github.com>
2026-08-29 21:47:58 +0530

    Add: anagram-groups - submission-11

---
+++ Data Structures & Algorithms/anagram-groups/submission-11.go
@@ -0,0 +1,22 @@
+func groupAnagrams(strs []string) [][]string {
+	groups := make(map[[26]int][]string) 
+
+	for _, word := range strs {
+		var count [26]int
+
+		for _, ch := range word {
+			count[ch - 'a']++
+		}
+		
+		// Add to the map
+		groups[count] = append(groups[count], word)
+	}
+
+	result := make([][]string, 0, len(groups))
+
+	for _, group := range groups {
+		result = append(result, group)
+	}
+
+	return result
+}
